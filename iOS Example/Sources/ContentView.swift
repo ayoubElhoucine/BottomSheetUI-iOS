e2992cfe43322431b9ad422f9801125a48399aa9
ayoubElhoucine <ayoubelhoucine@gmail.com>
2023-09-28 16:45:27 +0100

    add an example of using bottom sheet

--- iOS Example/Sources/ContentView.swift
+++ iOS Example/Sources/ContentView.swift
@@ -27,7 +27,35 @@ struct ContentView: View {
         }
         .background(.white)
         .asBottomSheetUI(show: $showDialog) {
-            Text("dialog conent")
+            VStack(spacing: 20) {
+                HStack {
+                    Spacer()
+                    Text("Bottom Sheet title").foregroundColor(.black).padding(.leading, 50).font(.system(size: 18, weight: Font.Weight.semibold))
+                    Spacer()
+                    Image(systemName: "xmark.circle.fill")
+                        .resizable()
+                        .frame(width: 22, height: 22)
+                        .foregroundColor(.black.opacity(0.6))
+                        .padding(16)
+                        .onTapGesture {
+                            showDialog = false
+                        }
+                }
+                ForEach(0 ..< 3) { index in
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack {
+                            Spacer().frame(width: 16)
+                            ForEach(0..<6) { index in
+                                Rectangle().fill(.gray.opacity(0.3)).frame(width: 90, height: 90).cornerRadius(8)
+                            }
+                        }
+                    }.frame(height: 90)
+                }
+                Spacer().frame(height: 50)
+            }
+            .background(.white)
+            .clipShape(RoundedRectangle(cornerRadius: 16))
+            .shadow(radius: 5)
         }
     }
 }
