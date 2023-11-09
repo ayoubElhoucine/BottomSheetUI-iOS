8dd5caadf46356470d58df7549020b00198f3dbb
RMehdid <s.mahdid@univ-boumerdes.dz>
2023-11-09 15:31:49 +0100

    try to fix frame

--- Sources/View/BottomSheetViewModifier.swift
+++ Sources/View/BottomSheetViewModifier.swift
@@ -28,7 +28,7 @@ public struct BottomSheetViewModifier<ContentView: View>: ViewModifier {
             if show {
                 Spacer()
                     .zIndex(1)
-                    .frame(width: .infinity, height: .infinity)
+                    .frame(minWidth: .infinity, minHeight: .infinity)
                     .background(Color.black.opacity(0.4).ignoresSafeArea())
                     .animation(.default, value: self.show)
                     .onTapGesture {
